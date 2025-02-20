e81a94b047ffa14eae0f1e7cd0ad125df32ad20d
Neelesh Vashist <neeleshvashist@gmail.com>
2025-02-19 23:52:36 -0500

    Deployment Commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
-import Skills from "./components/skills";
+import Skills from "./components/Skills";
 import Education from "./components/Education";
 import GoToTopButton from "./components/GoToTopButton";
 import AboutMe from "./components/About";
